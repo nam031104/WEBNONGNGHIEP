f01f91229e20d6f4878f6101242832e1a246a903
NgocQuoc <quocnui29@gmail.com>
2026-04-03 00:22:39 +0700

    Thay đổi package

--- BE/src/main/java/btl/nongnghiep/BeLoginApplication.java
+++ BE/src/main/java/btl/nongnghiep/BeLoginApplication.java
@@ -2,14 +2,12 @@ package btl.nongnghiep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "btl.nongnghiep")
+
 public class BeLoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeLoginApplication.class, args);
     }
 }
-
